13b6676463eb7077f1f1a0730032750647ff1a73
cyberr27 <aleksejbalinskij27@gmail.com>
2025-04-04 22:44:47 +0300

    !

--- public/code.js
+++ public/code.js
@@ -579,7 +579,7 @@ function update(deltaTime) {
 
   // Обновление анимации и отправка данных
   if (moved && !checkCollision(me.x, me.y)) {
-    me.steps += deltaTime / 1000; // Шаги пропорциональны времени
+    me.steps += deltaTime / 20; // Шаги пропорциональны времени
     updateResources();
 
     if (me.state === "walking") {
@@ -641,9 +641,9 @@ function update(deltaTime) {
 function updateResources() {
   const me = players.get(myId);
   if (!me) return;
-  if (me.steps % 100 === 0) me.energy = Math.max(0, me.energy - 1);
-  if (me.steps % 50 === 0) me.food = Math.max(0, me.food - 1);
-  if (me.steps % 25 === 0) me.water = Math.max(0, me.water - 1);
+  if (me.steps % 300 === 0) me.energy = Math.max(0, me.energy - 1);
+  if (me.steps % 150 === 0) me.food = Math.max(0, me.food - 1);
+  if (me.steps % 75 === 0) me.water = Math.max(0, me.water - 1);
   if (
     (me.energy === 0 || me.food === 0 || me.water === 0) &&
     me.steps % 10 === 0
