f57bf0d176493e5c2c15b3ce6f7a1e61e18d1d44
cyberr27 <aleksejbalinskij27@gmail.com>
2025-04-12 17:49:36 +0300

    +

--- public/code.js
+++ public/code.js
@@ -79,68 +79,6 @@ driedFishImage.src = "dried_fish.png";
 const balyaryImage = new Image();
 balyaryImage.src = "balyary.png"; // Укажи правильный путь к файлу
 
-// Проверка загрузки новых изображений
-cannedMeatImage.onload = () => {
-  console.log("Банка тушёнки загружена");
-  onImageLoad();
-};
-cannedMeatImage.onerror = () =>
-  console.error("Ошибка загрузки canned_meat.png");
-mushroomImage.onload = () => {
-  console.log("Гриб загружен");
-  onImageLoad();
-};
-mushroomImage.onerror = () => console.error("Ошибка загрузки mushroom.png");
-sausageImage.onload = () => {
-  console.log("Колбаса загружена");
-  onImageLoad();
-};
-sausageImage.onerror = () => console.error("Ошибка загрузки sausage.png");
-bloodPackImage.onload = () => {
-  console.log("Пакет крови загружен");
-  onImageLoad();
-};
-bloodPackImage.onerror = () => console.error("Ошибка загрузки blood_pack.png");
-breadImage.onload = () => {
-  console.log("Хлеб загружен");
-  onImageLoad();
-};
-breadImage.onerror = () => console.error("Ошибка загрузки bread.png");
-vodkaBottleImage.onload = () => {
-  console.log("Водка загружена");
-  onImageLoad();
-};
-vodkaBottleImage.onerror = () =>
-  console.error("Ошибка загрузки vodka_bottle.png");
-meatChunkImage.onload = () => {
-  console.log("Кусок мяса загружен");
-  onImageLoad();
-};
-meatChunkImage.onerror = () => console.error("Ошибка загрузки meat_chunk.png");
-bloodSyringeImage.onload = () => {
-  console.log("Шприц с кровью загружен");
-  onImageLoad();
-};
-bloodSyringeImage.onerror = () =>
-  console.error("Ошибка загрузки blood_syringe.png");
-milkImage.onload = () => {
-  console.log("Молоко загружено");
-  onImageLoad();
-};
-milkImage.onerror = () => console.error("Ошибка загрузки milk.png");
-condensedMilkImage.onload = () => {
-  console.log("Сгущёнка загружена");
-  onImageLoad();
-};
-condensedMilkImage.onerror = () =>
-  console.error("Ошибка загрузки condensed_milk.png");
-
-balyaryImage.onload = () => {
-  console.log("Баляры загружены");
-  onImageLoad();
-};
-balyaryImage.onerror = () => console.error("Ошибка загрузки balyary.png");
-
 // Инвентарь игрока (массив на 20 слотов, изначально пустой)
 let inventory = Array(20).fill(null);
 
