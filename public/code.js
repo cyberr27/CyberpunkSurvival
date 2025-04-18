6697350bdd996713a3a1c6d83a83890bf188d1a4
cyberr27 <aleksejbalinskij27@gmail.com>
2025-04-18 22:43:55 +0300

    +

--- public/code.js
+++ public/code.js
@@ -98,16 +98,19 @@ const ITEM_CONFIG = {
     effect: { energy: 20, water: 5 },
     image: energyDrinkImage,
     description: "Энергетик: +20 эн. +5 воды.",
+    rarity: 3,
   },
   nut: {
     effect: { food: 7 },
     image: nutImage,
     description: "Орех: +7 еды.",
+    rarity: 3,
   },
   water_bottle: {
     effect: { water: 30 },
     image: waterBottleImage,
     description: "Вода: +30 воды.",
+    rarity: 3,
   },
   apple: {
     effect: { food: 8, water: 5 },
@@ -131,56 +134,67 @@ const ITEM_CONFIG = {
     effect: { food: 20 },
     image: cannedMeatImage,
     description: "Банка тушёнки: +20 еды.",
+    rarity: 1,
   },
   mushroom: {
     effect: { food: 5, energy: 15 },
     image: mushroomImage,
     description: "Гриб прущий: +15 энергии. +5 еды.",
+    rarity: 1,
   },
   sausage: {
     effect: { food: 16, energy: 3 },
     image: sausageImage,
     description: "Колбаса: +16 еды, +3 энергии.",
+    rarity: 2,
   },
   blood_pack: {
     effect: { health: 40 },
     image: bloodPackImage,
     description: "Пакет крови: +40 здоровья.",
+    rarity: 1,
   },
   bread: {
     effect: { food: 13, water: -2 },
     image: breadImage,
     description: "Хлеб: +13 еды, -2 воды.",
+    rarity: 2,
   },
   vodka_bottle: {
     effect: { health: 5, energy: -2, water: 1, food: 2 },
     image: vodkaBottleImage,
     description: "Водка: +5 здоровья, -2 эн. +1 воды, +2 еды.",
+    rarity: 2,
   },
   meat_chunk: {
     effect: { food: 20, energy: 5, water: -2 },
     image: meatChunkImage,
     description: "Кусок мяса: +20 еды, +5 эн. -2 воды.",
+    rarity: 2,
   },
   blood_syringe: {
     effect: { health: 10 },
     image: bloodSyringeImage,
     description: "Шприц с кровью: +10 здоровья.",
+    rarity: 2,
   },
   milk: {
     effect: { water: 15, food: 5 },
     image: milkImage,
     description: "Молоко: +15 воды, +5 еды.",
+    rarity: 2,
   },
   condensed_milk: {
     effect: { water: 5, food: 11, energy: 2 },
     image: condensedMilkImage,
     description: "Сгущёнка: +11 еды, +5 воды, +2 эн.",
+    rarity: 2,
   },
   dried_fish: {
     effect: { food: 10, water: -3 },
     image: driedFishImage,
     description: "Сушёная рыба: +10 еды, -3 воды.",
+    rarity: 2,
   },
   balyary: {
     effect: {}, // Эффекта нет, это валюта
