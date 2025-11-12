d362a4dfbca93d1badf7fd8ba913a810b65b85bc
cyberr27 <aleksejbalinskij27@gmail.com>
2025-11-12 19:10:16 +0200

    +

--- public/code.js
+++ public/code.js
@@ -759,11 +759,7 @@ function startGame() {
         const currentWorldId = window.worldSystem.currentWorldId;
         let selectedPlayerId = null;
         players.forEach((player, id) => {
-          if (
-            id !== myId &&
-            player.health > 0 &&
-            player.worldId === currentWorldId
-          ) {
+          if (id !== myId && player.worldId === currentWorldId) {
             const dx = worldX - (player.x + 20);
             const dy = worldY - (player.y + 20);
             if (Math.sqrt(dx * dx + dy * dy) < 40) {
@@ -814,11 +810,7 @@ function startGame() {
       const currentWorldId = window.worldSystem.currentWorldId;
       let selectedPlayerId = null;
       players.forEach((player, id) => {
-        if (
-          id !== myId &&
-          player.health > 0 &&
-          player.worldId === currentWorldId
-        ) {
+        if (id !== myId && player.worldId === currentWorldId) {
           const dx = worldX - (player.x + 20);
           const dy = worldY - (player.y + 20);
           if (Math.sqrt(dx * dx + dy * dy) < 40) {
