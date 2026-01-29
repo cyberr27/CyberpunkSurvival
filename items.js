3eee205fd007b63efe8c4650efeb1644341e0f0a
cyberr27 <aleksejbalinskij27@gmail.com>
2026-01-29 10:04:32 +0200

    +

--- items.js
+++ items.js
@@ -513,21 +513,19 @@ const ITEM_CONFIG = {
   },
 
   recipe_torn_equipment: {
-    effect: {},
     description:
       "Рецепт порванной экипировки — материал для улучшений у Торестоса",
     rarity: 1,
     stackable: true,
-    recipe: "recipe_torn_equipment"
+    recipe: "recipe_torn_equipment",
   },
 
   recipe_chameleon_equipment: {
-    effect: {},
     description:
       "Рецепт хамелеон-экипировки — материал для улучшений у Торестоса",
     rarity: 1,
     stackable: true,
-    recipe: "recipe_chameleon_equipment"
+    recipe: "recipe_chameleon_equipment",
   },
 };
 
