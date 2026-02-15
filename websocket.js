c7a908f74aa7fdd1bda40b3bf4f6285738e8617f
cyberr27 <aleksejbalinskij27@gmail.com>
2026-02-15 08:53:21 +0200

    +

--- websocket.js
+++ websocket.js
@@ -854,7 +854,7 @@ function setupWebSocket(
               selectedQuestId: playerData.selectedQuestId,
               level: playerData.level,
               xp: playerData.xp,
-              meleeDamageBonus: playerData.maleeDamageBonus || 0,
+              meleeDamageBonus: playerData.meleeDamageBonus || 0,
               skills: playerData.skills,
               skillPoints: playerData.skillPoints,
               upgradePoints: playerData.upgradePoints,
